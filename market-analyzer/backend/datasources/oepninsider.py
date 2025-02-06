b4a99ebd5a3f7e8142e597d05aab313782ee0b8c
joaomalho <113609805+joaomalho@users.noreply.github.com>
2025-02-06 22:01:39 +0000

    WIP Open Insider Scraper

---
+++ market-analyzer/backend/datasources/oepninsider.py
@@ -0,0 +1,16 @@
+
+
+
+class DataOpenInsider():
+
+    def __init__(self) -> None:
+        self
+
+    def get_openinsider_data(self, symbol : str):
+        '''
+        This method returns the data from openinsider.com
+        '''
+
+        
+
+
